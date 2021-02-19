52372d258d873e0fa8687b3a407b4c869be25f99
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-19 18:16:25 -0500

    Initial Commit

---
+++ Braket Tours/Braket_ToursApp.swift
@@ -0,0 +1,17 @@
+//
+//  Braket_ToursApp.swift
+//  Braket Tours
+//
+//  Created by Vidur Khanal on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Braket_ToursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Braket Tours/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Braket Tours
+//
+//  Created by Vidur Khanal on 19/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
