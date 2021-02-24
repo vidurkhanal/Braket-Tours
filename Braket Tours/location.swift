aadb22aa5104ec6fb30fe578739b521351e66a14
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-23 21:15:16 -0500

    LIBRARY FOR DATA FECTHING CREATED

--- Braket Tours/location.swift
+++ Braket Tours/location.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Location {
+    let id:Int
+    let name:String
+    let country:String
+    let description:String
+    let more:String
+    let latitude:Double
+    let longitude:Double
+    let heroPicture:String
+    let advisory:String
+    
+    static let example = Location(id:1,name: "The Taj Mahal",country: "India",description: "The Taj Mahal is an ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan (reigned from 1628 to 1658) to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself. The tomb is the centrepiece of a 17-hectare (42-acre) complex, which includes a mosque and a guest house, and is set in formal gardens bounded on three sides by a crenellated wall.",more: "More Text",latitude: 35.652,longitude: -83.5070,heroPicture: "smokies",advisory: "Beware Of the bears!")
+}
